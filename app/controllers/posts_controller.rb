9d5d2b976ca1d77008409eeb9008e3906beb5dd7
Strawhub <k7to.so23.ten717@i.softbank.jp>
2023-01-11 01:47:42 +0900

    Add: delete lwink

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,8 +35,14 @@ class PostsController < ApplicationController
         else
             render 'edit'
         end
-
     end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
+    end
+
 
     private
     def post_params
