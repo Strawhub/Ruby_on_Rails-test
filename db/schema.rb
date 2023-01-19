17fc8c23e2426e5ca9700b407e25b9ecf38739ff
Strawhub <k7to.so23.ten717@i.softbank.jp>
2023-01-19 13:47:00 +0900

    change: Delete by link

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_14_135521) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_19_003703) do
   create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "body"
     t.bigint "post_id", null: false
